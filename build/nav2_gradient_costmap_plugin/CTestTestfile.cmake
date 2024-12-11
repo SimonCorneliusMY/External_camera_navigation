096307bc9a46da627d5df3fee4319291a50fa284
SPC <simoncornelius16@gmail.com>
2024-12-11 11:00:00 +0800

    track build plugins

---
+++ build/nav2_gradient_costmap_plugin/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tarumt2204/External_camera_navigation/src/nav2_gradient_costmap_plugin
+# Build directory: /home/tarumt2204/External_camera_navigation/build/nav2_gradient_costmap_plugin
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
