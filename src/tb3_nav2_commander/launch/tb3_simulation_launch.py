00521df21eb57ba30c210cf642ec079e6542eb36
SPC <simoncornelius16@gmail.com>
2025-02-07 09:43:31 +0800

    original scripts uneditted

--- src/tb3_nav2_commander/launch/tb3_simulation_launch.py
+++ src/tb3_nav2_commander/launch/tb3_simulation_launch.py
@@ -28,7 +28,7 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     # Get the launch directory
-    bringup_dir = get_package_share_directory('nav2_bringup')
+    bringup_dir = get_package_share_directory('tb3_nav2_commander')
     launch_dir = os.path.join(bringup_dir, 'launch')
 
     # Create the launch configuration variables
@@ -96,7 +96,7 @@ def generate_launch_description():
 
     declare_params_file_cmd = DeclareLaunchArgument(
         'params_file',
-        default_value='/home/tarumt2204/desk_ws/src/tb3_nav2_commander/param/nav2_params.yaml',
+        default_value=os.path.join(bringup_dir, 'params', 'nav2_params.yaml'),
         description='Full path to the ROS2 parameters file to use for all launched nodes')
 
     declare_autostart_cmd = DeclareLaunchArgument(
