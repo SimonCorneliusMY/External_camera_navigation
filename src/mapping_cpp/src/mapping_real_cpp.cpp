c2ab0d5a901ac005f82094c1f9074c3a945f4d16
SPC <simoncornelius16@gmail.com>
2025-04-03 10:06:11 +0800

    include parameters, obstacle publisher in own function

--- src/mapping_cpp/src/mapping_real_cpp.cpp
+++ src/mapping_cpp/src/mapping_real_cpp.cpp
@@ -15,6 +15,11 @@
 #include <message_filters/time_synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
 #include "my_custom_msgs/msg/bbox.hpp"
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <geometry_msgs/msg/transform_stamped.hpp>
+#include <geometry_msgs/msg/point_stamped.hpp>
 
 
 #include <iostream>
@@ -88,18 +93,29 @@ public:
     {
         this->declare_parameter("save_map", false);
         this->declare_parameter<bool>("show_fps", false);
+        this->declare_parameter<std::string>("name", "0");
+        this->declare_parameter<double>("resolution", 3.45787/1112);
+        this->declare_parameter<std::vector<int>>("homographic_ori_points", {636,73,1000, 77,171,531,1278,640});
+        this->declare_parameter<std::vector<int>>("homographic_transformed_points", {0,0,1112,0,0,2855,1112,2855});
+        this->declare_parameter<std::vector<int>>("HSV", {18, 0, 0, 28, 255, 255});
+        this->declare_parameter<int>("inflation", 17);
+        
+        this->get_parameter("resolution", map.info.resolution);
         this->get_parameter("save_map", save_map);
+        this->get_parameter("name", name);
+        pts_ori = this->get_parameter("homographic_ori_points").as_integer_array();
+        pts_homo = this->get_parameter("homographic_transformed_points").as_integer_array();
         rclcpp::QoS qos_profile_reliable(rclcpp::KeepLast(1));
         qos_profile_reliable.transient_local();
         qos_profile_reliable.reliable();
-        cv::namedWindow("image",cv::WINDOW_NORMAL);
-        cv::namedWindow("image2",cv::WINDOW_NORMAL);
-        cv::resizeWindow("image",600,1200);
-        cv::resizeWindow("image2",600,1200);
+        // cv::namedWindow("image",cv::WINDOW_NORMAL);
+        // cv::namedWindow("image2",cv::WINDOW_NORMAL);
+        // cv::resizeWindow("image",600,1200);
+        // cv::resizeWindow("image2",600,1200);
 
         // subscription topic using the sync package
-        image_sub.subscribe(this,"camera/image_raw");
-        bounding_box_sub.subscribe(this,"bounding_box");
+        image_sub.subscribe(this,"camera_" + name +  "/image_raw");
+        bounding_box_sub.subscribe(this,"bounding_box_" + name);
 
         uint32_t queue_size = 10;
 
@@ -113,19 +129,21 @@ public:
         sync->registerCallback(std::bind(&Mapping::SyncCallback, this, _1, _2));
 
         // normal ros2 publisher
-        map_publisher = this->create_publisher<nav_msgs::msg::OccupancyGrid>("map", qos_profile_reliable);
-        obstacle_publisher = this->create_publisher<sensor_msgs::msg::PointCloud2>("obstacle", qos_profile_reliable);
+        map_publisher = this->create_publisher<nav_msgs::msg::OccupancyGrid>("map_" + name , qos_profile_reliable);
+        obstacle_publisher = this->create_publisher<sensor_msgs::msg::PointCloud2>("obstacle_" + name , qos_profile_reliable);
+        tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+        tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
 
         br = std::make_shared<cv_bridge::CvImage>();
         
-
+        
         // Specify the 4 corner coordinates (Z-shape in row-major order)
-        pts1 = { cv::Point2f(599, 98), cv::Point2f(938, 104), 
-                                        cv::Point2f(72, 532), cv::Point2f(1275, 637) };
-        pts2 = { cv::Point2f(0, 0), cv::Point2f(1203, 0), 
-                                        cv::Point2f(0, 2869), cv::Point2f(1203, 2869) };
+        pts1 = { cv::Point2f(pts_ori[0], pts_ori[1]), cv::Point2f(pts_ori[2], pts_ori[3]), 
+                                        cv::Point2f(pts_ori[4], pts_ori[5]), cv::Point2f(pts_ori[6], pts_ori[7]) };
+        pts2 = { cv::Point2f(pts_homo[0], pts_homo[1]), cv::Point2f(pts_homo[2], pts_homo[3]), 
+                                        cv::Point2f(pts_homo[4], pts_homo[5]), cv::Point2f(pts_homo[6], pts_homo[7]) };
 
-        // Calculate perspective transform matrix (done once during object creation)
+
         M = cv::getPerspectiveTransform(pts1, pts2);
 
     }
@@ -145,6 +163,7 @@ private:
             {
 
                 mapping(cv_ptr->image, bounding_box);
+                obstacles();
             }
         }
         catch (const cv_bridge::Exception &e)
@@ -153,127 +172,9 @@ private:
         }
         
     }
-    // void image_callback(const sensor_msgs::msg::Image::SharedPtr msg)
-    // {
-    //     try
-    //     {
-
-    //         cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-    //         // cv::Mat image = br->imgmsg_to_cv2(*msg, "bgr8");
-    //         if (!bounding_box.empty() && !cv_ptr->image.empty())
-    //         {
-    //             mapping(cv_ptr->image, bounding_box);
-    //             bbox_image_check = true;
-
-    //         }
-    //         else if (bbox_image_check == true)
-    //         {
-    //             RCLCPP_INFO(this->get_logger(), "No image or bounding box");
-    //             bbox_image_check = false;
-    //             return;
-    //         }
-
-            
-            
-    //     }
-    //     catch (const cv_bridge::Exception &e)
-    //     {
-    //         RCLCPP_ERROR(this->get_logger(), "Error converting image: %s", e.what());
-    //     }
-    // }
-
-    // void bounding_box_callback(const my_custom_msgs::msg::Bbox::SharedPtr msg)
-    // {
-    //     bounding_box = msg->data;
-
-    //     // Printing the entire array (elements) 
-    //     // RCLCPP_INFO(this->get_logger(), "Received data:");
-    //     //     for (size_t i = 0; i < msg->data.size(); ++i)
-    //     //     {
-    //     //         RCLCPP_INFO(this->get_logger(), "Element %zu: %d", i, msg->data[i]);
-    //     //     }
-
-    // }
-    // TODO the thresholding has some weird issue where causing the mask to have multiple value
-    void mapping(const cv::Mat &image,  std::vector<int16_t> &bounding_box)
-    {
-        // to show fps of node
-        bool show_fps = this -> get_parameter("show_fps").get_value<bool>();
-        if (show_fps == true){
-            FPSCounter::FPSStats stats = counter.fps();
-            if (stats.fps_value != -1)
-            {
-                RCLCPP_INFO(this->get_logger(), "FPS: %d, Min: %f, Max: %f, Avg: %f",stats.fps_value,stats.min_time,stats.max_time,stats.avg_time);
-            }
-        }
-
-        // Threshold values to isolate floor
-        cv::cvtColor(image, hsv, cv::COLOR_BGR2HSV);
-        cv::inRange(hsv, cv::Scalar(10, 0, 0), cv::Scalar(30, 255, 255), mask);
-
-        // Blackout the TurtleBot3 pose, bounding_box is top left and bottom right coordinates, each box requires 4 values
-        for (size_t i = 0; i < bounding_box.size(); i += 4)
-            {
-                int x = bounding_box[i]-TB3_pixel_inflation;
-                int y = bounding_box[i + 1]-TB3_pixel_inflation;
-                int width = bounding_box[i + 2] - bounding_box[i]+TB3_pixel_inflation*2;
-                int height = bounding_box[i + 3] - bounding_box[i + 1]+TB3_pixel_inflation*2;
-
-                // Create a cv::Rect for each bounding box
-                cv::Rect bbox(x, y, width, height);
-
-                // Black out the area in the image corresponding to the bounding box
-                cv::rectangle(mask, bbox, cv::Scalar(255, 255, 255), cv::FILLED);
-        }
-
-        // Apply the perspective transform
-        cv::warpPerspective(mask, homo_transform, M, cv::Size(1203, 2869));
-        // make it a mask again because interpolation in warpPerspective
-        cv::threshold(homo_transform,homo_transform,cv::THRESH_BINARY_INV | cv::THRESH_OTSU,100,cv::ThresholdTypes::THRESH_BINARY_INV);
-
-        // Create a set to store unique values, keep for checking unique value 250206 Simon
-        // std::set<int> uniqueValues;
-
-        // // Iterate through each element in the matrix and add it to the set
-        // for (int i = 0; i < mask.rows; ++i) {
-        //     for (int j = 0; j < mask.cols; ++j) {
-        //         uniqueValues.insert(mask.at<uchar>(i, j));
-        //     }
-        // }
-        // for (int val : uniqueValues){
-        //     RCLCPP_INFO(this->get_logger(), "%d",val);
-        // }
-
-        cv::imshow("image2",homo_transform);
-        cv::imshow("image",mask);
-        cv::waitKey(1);
-        // Flip the mask (ROS map data has the origin at bottom-left, OpenCV is top-left)
-        cv::flip(homo_transform, maze_bw_flip, 0);
-
-        // Update the map data and publish
-
-        map.header.frame_id = "map";
-        map.header.stamp = this->get_clock()->now();
-        map.info.height = maze_bw_flip.rows;
-        map.info.width = maze_bw_flip.cols;
-        map.info.resolution = 3.54/1203; // 3.54/1203
-        map.info.origin.orientation.w = 1.0;
-        map.info.origin.position.x = 0.0;
-        map.info.origin.position.y = 0.0;
-
-        //Too lazy to write a proper functions to save images
-        if (save_map)
-        {
-            cv::imwrite("/home/tarumt2204/External_camera_navigation/maze_100.pgm", homo_transform);
-            RCLCPP_INFO(this->get_logger(), "Image saved");
-            save_map = false;
-        }
-
-        // Flatten the map and publish
-        map.data.assign(maze_bw_flip.begin<int8_t>(), maze_bw_flip.end<int8_t>());
-        map_publisher->publish(map);
-
-
+    void obstacles(){
+        // clear points to avoid build up of data in it.
+        points.clear();
         
         // Set the header of the point cloud
         obstacle.header.stamp = rclcpp::Clock().now();
@@ -296,7 +197,7 @@ private:
                 {
 
                     // Convert grid coordinates (x, y) to world coordinates (X, Y)
-                    geometry_msgs::msg::Point32 pt;
+
                     pt.x = origin.position.x + x * resolution + resolution / 2.0;
                     pt.y = origin.position.y + y * resolution + resolution / 2.0;
                     pt.z = 0.0;  // Z is always 0 in 2D grid
@@ -307,24 +208,22 @@ private:
         }
         obstacle.width = points.size();  // One point for each cell in the grid
         // Create PointFields for the point cloud
-        std::vector<sensor_msgs::msg::PointField> fields;
+        
 
         // Add X, Y, Z field (for 3D point cloud data)
-        sensor_msgs::msg::PointField x_field;
+
         x_field.name = "x";
         x_field.offset = 0;
         x_field.datatype = sensor_msgs::msg::PointField::FLOAT32;
         x_field.count = 1;
         fields.push_back(x_field);
 
-        sensor_msgs::msg::PointField y_field;
         y_field.name = "y";
         y_field.offset = 4;  // Offset in bytes (float is 4 bytes)
         y_field.datatype = sensor_msgs::msg::PointField::FLOAT32;
         y_field.count = 1;
         fields.push_back(y_field);
 
-        sensor_msgs::msg::PointField z_field;
         z_field.name = "z";
         z_field.offset = 8;
         z_field.datatype = sensor_msgs::msg::PointField::FLOAT32;
@@ -339,29 +238,117 @@ private:
         obstacle.row_step = obstacle.point_step * points.size();
         obstacle.is_bigendian = false; // Little endian
         obstacle.is_dense = true; // All points are valid (no NaNs or invalid points)
+
         
+
         // Create a buffer for the point data
         obstacle.data.resize(obstacle.row_step);
 
-        // Fill the data array with point information
+
         size_t i = 0;
         for (const auto& pt : points)
         {
-        memcpy(&obstacle.data[i], &pt.x, sizeof(pt.x));  // Copy x
-        i += sizeof(pt.x);
-        memcpy(&obstacle.data[i], &pt.y, sizeof(pt.y));  // Copy y
-        i += sizeof(pt.y);
-        memcpy(&obstacle.data[i], &pt.z, sizeof(pt.z));  // Copy z
-        i += sizeof(pt.z);
+            memcpy(&obstacle.data[i], &pt.x, sizeof(pt.x));  // Copy x
+            i += sizeof(pt.x);
+            memcpy(&obstacle.data[i], &pt.y, sizeof(pt.y));  // Copy y
+            i += sizeof(pt.y);
+            memcpy(&obstacle.data[i], &pt.z, sizeof(pt.z));  // Copy z
+            i += sizeof(pt.z);
         }
 
         // Publish the PointCloud2 message
         obstacle_publisher->publish(obstacle);
 
     }
+    void mapping(const cv::Mat &image,  std::vector<int16_t> &bounding_box)
+    {
+
+        // to show fps of node
+        bool show_fps = this -> get_parameter("show_fps").get_value<bool>();
+        if (show_fps == true){
+            FPSCounter::FPSStats stats = counter.fps();
+            if (stats.fps_value != -1)
+            {
+                RCLCPP_INFO(this->get_logger(), "FPS: %d, Min: %f, Max: %f, Avg: %f",stats.fps_value,stats.min_time,stats.max_time,stats.avg_time);
+            }
+        }
+
+        // Threshold values to isolate floor
+        hsv_values = this->get_parameter("HSV").as_integer_array();
+        cv::cvtColor(image, hsv, cv::COLOR_BGR2HSV);
+        cv::inRange(hsv, cv::Scalar(hsv_values[0], hsv_values[1], hsv_values[2]), cv::Scalar(hsv_values[3], hsv_values[4], hsv_values[5]), mask);
+
+        TB3_pixel_inflation = this->get_parameter("inflation").get_value<int>();
+        // Blackout the TurtleBot3 pose, bounding_box is top left and bottom right coordinates, each box requires 4 values
+        for (size_t i = 0; i < bounding_box.size(); i += 4)
+            {
+                
+                int x = bounding_box[i]-TB3_pixel_inflation;
+                int y = bounding_box[i + 1]-TB3_pixel_inflation;
+                int width = bounding_box[i + 2] - bounding_box[i]+TB3_pixel_inflation*2;
+                int height = bounding_box[i + 3] - bounding_box[i + 1]+TB3_pixel_inflation*2;
+
+                // Create a cv::Rect for each bounding box
+                cv::Rect bbox(x, y, width, height);
+
+                // Black out the area in the image corresponding to the bounding box
+                cv::rectangle(mask, bbox, cv::Scalar(255, 255, 255), cv::FILLED);
+        }
+        
+        // Apply the perspective transform
+        cv::warpPerspective(mask, homo_transform, M, cv::Size(pts_homo[6], pts_homo[7]));
+        // make it a mask again because interpolation in warpPerspective
+        cv::threshold(homo_transform,homo_transform,cv::THRESH_BINARY_INV | cv::THRESH_OTSU,100,cv::ThresholdTypes::THRESH_BINARY_INV);
+
+        // Create a set to store unique values, keep for checking unique value 250206 Simon
+        // std::set<int> uniqueValues;
+
+        // // Iterate through each element in the matrix and add it to the set
+        // for (int i = 0; i < mask.rows; ++i) {
+        //     for (int j = 0; j < mask.cols; ++j) {
+        //         uniqueValues.insert(mask.at<uchar>(i, j));
+        //     }
+        // }
+        // for (int val : uniqueValues){
+        //     RCLCPP_INFO(this->get_logger(), "%d",val);
+        // }
+
+        // cv::imshow("image2",homo_transform);
+        // cv::imshow("image",mask);
+        // cv::waitKey(1);
+        // Flip the mask (ROS map data has the origin at bottom-left, OpenCV is top-left)
+        // cv::flip(homo_transform, maze_bw_flip, 0);
+       
+        map.header.frame_id = "map_" + name;
+        map.header.stamp = this->get_clock()->now();
+        map.info.height = homo_transform.rows;
+        map.info.width = homo_transform.cols;
+        // map.info.resolution = 3.54/1203; // 3.54/1203
+        // map.info.origin.orientation.x = transform.transform.rotation.x;;
+        // map.info.origin.orientation.y = transform.transform.rotation.y;;
+        // map.info.origin.orientation.z = transform.transform.rotation.z;;
+        map.info.origin.orientation.w = 1.0;;
+        map.info.origin.position.x = 0.0;
+        map.info.origin.position.y = 0.0;
+
+        //Too lazy to write a proper functions to save images
+        if (save_map)
+        {
+            cv::imwrite("/home/tarumt2204/External_camera_navigation/maze_100.pgm", homo_transform);
+            RCLCPP_INFO(this->get_logger(), "Image saved");
+            save_map = false;
+        }
+        
+        // Flatten the map and publish
+        map.data.assign(homo_transform.begin<int8_t>(), homo_transform.end<int8_t>());
+        map_publisher->publish(map);
+
+    }
 
     rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr map_publisher;
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr obstacle_publisher;
+    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
     
     
     sensor_msgs::msg::PointCloud2 obstacle;
@@ -369,19 +356,26 @@ private:
     nav_msgs::msg::OccupancyGrid map;
     std::shared_ptr<cv_bridge::CvImage> br;
     std::shared_ptr<FPSCounter> fps_counter;
-    
 
+    // PointCloud2 variables
+    sensor_msgs::msg::PointField x_field, y_field, z_field;
+    std::vector<sensor_msgs::msg::PointField> fields;
+    geometry_msgs::msg::Point32 pt;
+    
+    std::string name;
     bool save_map,bbox_image_check;
     std::vector<int16_t> bounding_box;
+
     cv::Point2f pose_xy_homo;
     cv::Point2f pose_xy_pixels_homo;
-    bool show_homographic_region = true;
-    float homo_resolution = 0.1;
+    
+
+    std::chrono::steady_clock::time_point t1, t2, t3 ;
     cv::Mat homo_transform, M, hsv, mask, mask_open, maze_bw_flip;
-    std::vector<cv::Point2f> pts1;
-    std::vector<cv::Point2f> pts2;
+    std::vector<cv::Point2f> pts1,pts2;
     FPSCounter counter;
-    int TB3_pixel_inflation = 50;   //erases the surrounding pixels
+    int TB3_pixel_inflation, size = 0;   //erases the surrounding pixels
+    std::vector<int64_t> pts_ori, pts_homo, hsv_values;
 
     message_filters::Subscriber<my_custom_msgs::msg::Bbox> bounding_box_sub;
     message_filters::Subscriber<sensor_msgs::msg::Image> image_sub;
